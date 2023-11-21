13d82e62efc81ae32cb0e2141c4b9a300b5513cc
Hong Phong <hongphong2000p@gmail.com>
2023-11-21 21:56:23 +0700

    build

--- build/config/config.js
+++ build/config/config.js
@@ -24,10 +24,21 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    // username: "root",
+    // password: null,
+    // database: "database_production",
+    // host: "127.0.0.1",
+    // dialect: "mysql",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    define: {
+      freezeTableName: true
+    },
+    logging: false,
+    timezone: "+07:00"
   }
 };
