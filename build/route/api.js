c3b046c8149b98db7eb6793d9cb9c784dc346445
Hong Phong <hongphong2000p@gmail.com>
2023-11-24 11:01:35 +0700

    q

--- build/route/api.js
+++ build/route/api.js
@@ -32,7 +32,8 @@ var initApiRouter = function initApiRouter(app) {
   }));
   router.get("/google/redirect", _passport["default"].authenticate("google"), function (req, res) {
     console.log("checkkkkkk reqqqqqqqq", req.user);
-    res.redirect("".concat(process.env.PORT_URL, "/FE-book-deploy/login"));
+    //res.redirect(`${process.env.PORT_URL}/FE-book-deploy/login`);
+    res.redirect("/");
   }
   // passport.authenticate("google", {
   //   // successRedirect: `${process.env.PORT_URL}/login`,
