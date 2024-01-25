81cbf9fb811ee963132d950afba9c20a24f84934
Hong Phong <hongphong2000p@gmail.com>
2024-01-25 08:21:34 +0700

    init

--- src/server.js
+++ src/server.js
@@ -4,10 +4,8 @@ import initApiRouter from "./route/api";
 import bodyParser from "body-parser";
 import connectDB from "./config/connectDB";
 import cookieParser from "cookie-parser";
-import configLoginWithSocial from "./controllers/socialController";
-import passport from "passport";
-import session from "express-session";
-//import cors from "./config/cors";
+
+
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 
@@ -18,28 +16,13 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 app.use(fileUpload());
+
 configViewEngine(app);
 
-// app.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: "SECRETSESSIONLG",
-//   })
-// );
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: true,
-    secret: "SECRETSESSIONLG",
-  })
-);
 
-app.use(passport.initialize());
-app.use(passport.session());
-//cors(app);
-//////
+
 app.use(
   cors({
     origin: process.env.PORT_URL,
@@ -50,11 +33,9 @@ app.use(
 
 connectDB();
 initApiRouter(app);
-configLoginWithSocial();
 
-// app.get("/images/book/:id", (req, res) => {
-//   res.render("image.ejs", { urlImage: req.params.id });
-// });
+
+
 app.listen(PORT, () => {
   console.log("run app success , port is: ", +PORT);
 });
